4000e6cd4dd85941d5de0d977107ea567a7dd5b2
kancell <2661030790@qq.com>
2021-06-26 23:26:36 +0800

    routes fix

--- config/routes.ts
+++ config/routes.ts
@@ -1,7 +1,19 @@
-import { SmileOutlined, CrownOutlined, TabletOutlined, AntDesignOutlined } from '@ant-design/icons';
+import {
+  SmileOutlined,
+  CrownOutlined,
+  TabletOutlined,
+  AntDesignOutlined,
+} from '@ant-design/icons';
 
 export default [
-  { exact: true, path: '/', name: '首页', component: 'index', icon: 'smile', },
-  { exact: true, path: '/chart', name: '图表', component: 'chart', icon: 'Crown', access: 'canReadPageA' },
-  { exact: true, path: '/map', name: '地图', component: 'Tablet' },
+  { exact: true, path: '/', name: '首页', component: 'index', icon: 'smile' },
+  {
+    exact: true,
+    path: '/chart',
+    name: '图表',
+    component: 'chart',
+    icon: 'Crown',
+    access: 'canReadPageA',
+  },
+  { exact: true, path: '/map', name: '地图', component: 'map', icon: 'Tablet' },
 ];
